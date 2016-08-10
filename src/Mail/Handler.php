b950cf1a4ef9b9da54a289e748c6372cda7aa1c1
Wouter Lemcke <w.lemcke@coolblue.nl>
2016-08-10 18:42:12 +0200

    fix mail handler

--- src/Mail/Handler.php
+++ src/Mail/Handler.php
@@ -7,14 +7,15 @@ abstract class Handler
     public static function mail(Message $message)
     {
         $headers = 'From: ' . $message->getSender() . "\r\n";
-        $headers .= 'Reply-To: contact@lemckewebsolutions.nl' . "\r\n";
+        $headers .= 'Reply-To: ' . $message->getSender() . "\r\n";
         $headers .= 'X-Mailer: PHP/' . phpversion();
 
         mail(
             $message->getReceiver(),
             $message->getSubject(),
             $message->getBody(),
-            $headers
+            $headers,
+            "-f " . $message->getSender()
         );
     }
 }
